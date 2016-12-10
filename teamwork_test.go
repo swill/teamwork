ab65bf1724f5c4d48ab806e10b74586a4243f7b5
Will Stevens <williamstevens@gmail.com>
2016-12-10 11:10:39 -0500

    added an example for Connect

---
+++ teamwork_test.go
@@ -0,0 +1,18 @@
+package teamwork
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/swill/teamwork"
+)
+
+func Example_Connect() {
+	// setup the teamwork connection
+	api_token := "the_teamwork_api_token"
+	conn, err := teamwork.Connect(api_token)
+	if err != nil {
+		fmt.Printf("Error connecting to TeamWork: %s", err.Error())
+		os.Exit(1)
+	}
+}
